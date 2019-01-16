a82b9ace11ea2051a2fff56858bb0779ae1e46f2
ratnakarmishra <ratnakarmishra786@gmail.com>
2019-01-16 18:46:43 +0530

    to show robin

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+class r
+{
+	int id,fees;
+	char name;
+};void main()
+{
+
+cout<<"enter your name plz";
+cin>>name;
+cout<<"enter fees";
+cin>>fees;
+cout<<fees<<name<<endl;
+getch();
+}
